6cc945994b5298ba35a4ae911cbc61c664ce3450
Fuh Suh <tofuhsuh@gmail.com>
2023-04-15 02:53:49 +0300

    corrected merge conflict

--- HospitalGPS/src/context/actions.js
+++
@@ -1,6 +0,0 @@
-export const SET_LOADING = "SET_LOADING";
-export const SET_HOSPITALS = "SET_HOSPITALS";
-export const HANDLE_PAGE = "HANDLE_PAGE";
-export const HANDLE_CITY = "HANDLE_CITY";
-export const HANDLE_ADDRESS = "HANDLE_ADDRESS";
-export const HANDLE_CODE = "HANDLE_CODE";
